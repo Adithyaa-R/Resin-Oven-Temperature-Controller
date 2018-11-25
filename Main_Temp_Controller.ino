01ac69905b73fc5b292784e89b98e3a2e8fe532d
Adithyaa <Adithyaa-R@users.noreply.github.com>
2018-11-25 20:41:16 +0530

    Main controller code
    WIP, testing if code uploads on secondary PC dedicated for uploading.

---
+++ Main_Temp_Controller.ino
@@ -0,0 +1,22 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x3F, 16, 2);//address, width, height.
+
+void setup()
+{
+  // initialize the LCD
+  lcd.begin();
+  lcd.backlight();
+
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  lcd.print("Hello, world!");
+  lcd.println("Temp- ");
+  delay(1000);
+  lcd.clear();
+  delay(2000);  
+}
