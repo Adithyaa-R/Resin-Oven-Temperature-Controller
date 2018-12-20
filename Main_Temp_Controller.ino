1fe1dd0d3f23b22c0112f4a513773ae39a58a2ff
Adithyaa <Adithyaa-R@users.noreply.github.com>
2018-12-20 09:25:54 +0530

    Update Main_Temp_Controller.ino

--- Main_Temp_Controller.ino
+++ Main_Temp_Controller.ino
@@ -1,44 +1,60 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-//#include <Adafruit_Sensor.h>
 //#include <DHT.h>
 
+
+#include <OneWire.h>
+#include <DS18B20.h>
+#define ONE_WIRE_BUS 4 //originally 2
+OneWire oneWire(ONE_WIRE_BUS);
+DS18B20 sensor(&oneWire);
+
+
 LiquidCrystal_I2C lcd(0x3F, 16, 2);//address, width, height.
+//DHT dht;
+
 int TargetTemp = 0;
 int CurrTemp = 0;
 int CurrHum = 0;
 
+int Hyst = 3;//the hysterisis of the temperature.
 int TempMin = 15;
 int TempMax = 80;
 
-int UpdateFreq = 5 * 1000;//seconds x millis
+int UpdateFreq = 5 * 1000;//seconds x millis //don't set below 1-2s, depending on temp sensor
 int lastUpdate = 0 - UpdateFreq;//subtracts the first update length so it runs first time.
+
+//pins
+int relayPin = 1;
+int potPin = 3;
+int DHTpin = 4;
+
 
 void setup()
 {
   // initialize the LCD
   lcd.begin();
   lcd.backlight();
-
+  //dht.setup(DHTpin);
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+  SetTemp();
+  LCD_Update();
   if (millis() > lastUpdate + UpdateFreq)
   {
     lastUpdate = millis();
     SetTemp();
     ReadTempDS18();
     //ReadTempDHT();
-    LCD_Update();
     Heater();
   }
 }
 
 void SetTemp()
 {
-  TargetTemp = map(analogRead(3), 0, 1023, TempMin, TempMax);
+  TargetTemp = map(analogRead(potPin), 0, 1023, TempMin, TempMax);
   TargetTemp = constrain(TargetTemp, TempMin, TempMax);
 }
 
@@ -48,12 +64,12 @@ void LCD_Update()
   lcd.setCursor(0, 0);
   lcd.print("Temp=");
   lcd.setCursor(6, 0);
-  lcd.print("xx");//info
+  lcd.print(CurrTemp);//info
 
   lcd.setCursor(10, 0);
   lcd.print("H=");//humidity label
   lcd.setCursor(13, 0);
-  lcd.print("xx");
+  lcd.print(CurrHum);
 
   lcd.setCursor(0, 1);
   lcd.print("Target=");
@@ -63,15 +79,25 @@ void LCD_Update()
 
 void ReadTempDS18()
 {
-
+  sensor.requestTemperatures();
+  while (!sensor.isConversionComplete());  // wait until sensor is ready
+  CurrTemp = sensor.getTempC();
 }
 
 void ReadTempDHT()
 {
-
+  //CurrHum = dht.getHumidity();
+  //CurrTemp = dht.getTemperature();
 }
 
 void Heater()
 {
-
+  if(CurrTemp <= TargetTemp)
+  {
+    digitalWrite(relayPin, HIGH);
+  }
+  else if(CurrTemp >= (TargetTemp + Hyst))
+  {
+    digitalWrite(relayPin, LOW);    
+  }
 }
